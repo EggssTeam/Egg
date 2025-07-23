315a8aae59552c20022fad71871c140989981044
GeFuyi <gefuyi927@gmail.com>
2025-07-23 22:42:03 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ DEEPSEEK_API_URL = "https://api.deepseek.com/v1/chat/completions"
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 
 # 宏定义 Whisper API 地址和你的API KEY
-WHISPER_API_URL = "https://api.gpt.ge/v1/audio/transcriptions"
-GPT4O_API_URL = "https://api.gpt.ge/v1/chat/completions"
+# WHISPER_API_URL = "https://api.gpt.ge/v1/audio/transcriptions"
+# GPT4O_API_URL = "https://api.gpt.ge/v1/chat/completions"
+WHISPER_API_URL = "https://api.openai.com/v1/audio/transcriptions"
+GPT4O_API_URL = "https://api.openai.com/v1/chat/completions"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
